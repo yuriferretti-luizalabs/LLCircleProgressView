ae1f0479c5384ef7cc1c2c4f3010162a23578875
Yuri Ferretti <yuri.ferretti@luizalabs.com>
2017-03-28 12:39:29 -0300

    Initial Commit

---
+++ CircleProgressView/CircleProgressView.h
@@ -0,0 +1,19 @@
+//
+//  CircleProgressView.h
+//  CircleProgressView
+//
+//  Created by Yuri Ferretti on 3/28/17.
+//  Copyright © 2017 yuri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CircleProgressView.
+FOUNDATION_EXPORT double CircleProgressViewVersionNumber;
+
+//! Project version string for CircleProgressView.
+FOUNDATION_EXPORT const unsigned char CircleProgressViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CircleProgressView/PublicHeader.h>
+
+
